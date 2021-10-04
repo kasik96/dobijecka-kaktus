f017743f49813b3fb5244043a2f5cb0b8d26b408
Martin Kase <mail@kasemartin.cz>
2021-10-04 21:44:44 +0200

    Update

--- popup.js
+++ popup.js
@@ -1,16 +1,20 @@
 window.onload = function () {
-  chrome.runtime.sendMessage(
-    { name: "GET_DATA_FOR_POPUP" },
-    function (response) {
-      if (response && response.name == "GET_DATA_FOR_POPUP") {
-        writeDataToPopup(response.data);
+  document.getElementById("state").innerHTML = "Data se aktualizují...";
+  setTimeout(() => {
+    chrome.runtime.sendMessage(
+      { name: "GET_DATA_FOR_POPUP" },
+      function (response) {
+        if (response && response.name == "GET_DATA_FOR_POPUP") {
+          writeDataToPopup(response.data);
+        }
       }
-    }
-  );
+    );
+  }, 1500);
 };
 
 function writeDataToPopup(data) {
   document.getElementById("text").innerHTML = data.text;
+  document.getElementById("date").innerHTML = new Date().toLocaleString();
   document.getElementById("state").innerHTML = data.valid
     ? "Dobíječka je aktivní!"
     : "Dobíječka je neaktivní!";
